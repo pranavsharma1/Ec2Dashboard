822061ea55f686f57dcb49a9f5c74c42be0d61c8
Pranav Sharma <pranavsharma@unomaha.edu>
2018-11-25 00:49:22 -0600

    Update Path

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const express = require('express');
 const mockServerData = require('./server.mock.json');
 const app = express();
 
-app.use( express.static(__dirname + '\\dist\\ec2dash\\'));
+app.use( express.static(__dirname + '/dist/ec2dash/'));
 
 
-console.log((__dirname + '\\dist\\ec2dash\\'));
+//console.log((__dirname + '\\dist\\ec2dash\\'));
 
 //app.use(express.json());
 
@@ -18,7 +18,7 @@ app.get('/api/instances', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '\\dist\\ec2dash\\index.html')
+  res.sendFile(__dirname + '/dist/ec2dash/index.html')
 });
 
 const PORT = process.env.PORT || 3000;
